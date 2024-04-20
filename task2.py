dbd05d3d87bdea54a5c5925d9ff666100e65a1c2
227den <119138344+227den@users.noreply.github.com>
2024-04-21 00:10:23 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,16 @@
 import random
 
-
-def get_numbers_ticket(min, max, quantity):
-    if not 1 <= min <= max <= 1000 or quantity <= 0:
+def get_numbers_ticket(min_num, max_num, quantity):
+    if min_num < 1 or max_num > 1000 or quantity > (max_num - min_num + 1):
+        print("Числа введені не коректно")
         return []
 
-    numbers = set()
-    while len(numbers) < quantity:
-        numbers.add(random.randint(min, max))
+    numbers_set = set()
 
-    return sorted(list(numbers))
+    while len(numbers_set) < quantity:
+        numbers_set.add(random.randint(min_num, max_num))
 
+    return sorted(list(numbers_set))
 
-lottery_numbers = get_numbers_ticket(1, 1000, 16)
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
